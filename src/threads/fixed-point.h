f82edea2c4940c8ff41e4df06c3db1b7a7158f6f
Sojeong <k5k5s5s5j5j5@naver.com>
2024-04-03 22:01:16 +0900

    fixed-point.c, fixed-point.h: add fixed point operation code. - C does not have class!!

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,5 +1,6 @@
 #ifndef THREADS_FIXED_POINT_H
 #define THREADS_FIXED_POINT_H
+#define F (1 << 14)
 
 /* Operations of integer & fixed point numbers.
 
@@ -8,52 +9,37 @@
    x, y: fixed point numbers
    n: integer   */
 
-class fp
-{
-    private:
-        const int f = (1<<14);
-        int value;
+/* n to fixed point */
+int to_fp(int n);
 
-    public:
-        fp();
-        ~fp();
+/* x to integer rounding toward zero (floor) */
+int to_int(int x);
 
-        /* constructor */
-        void set_fp(int x);
+/* x to integer rounding to nearest (round) */
+int to_int_round(int x);
 
-        /* n to fixed point */
-        int to_fp(int n);
+/* add fixed point numbers x, y -> x+y */
+int add_fps(int x, int y);
 
-        /* x to integer rounding toward zero (floor) */
-        int to_int(int x);
+/* subtract fixed point y from fixed point x -> x-y */
+int sub_fps(int x, int y);
 
-        /* x to integer rounding to nearest (round) */
-        int to_int_round(int x);
+/* add fixed point x and int n -> x+n */
+int add_mix(int x, int n);
 
-        /* add fixed point numbers x,y -> x+y */
-        int add_fps(int x, int y);
+/* subtract int n from fixed point y -> x-n */
+int sub_mix(int x, int n);
 
-        /* subtract fixed point y from fixed point x -> x-y */
-        int sub_fps(int x, int y);
+/* multiply fixed point x by fixed point y -> x*y */
+int multiply_fps(int x, int y);
 
-        /* add fixed point x and int n -> x+n */
-        int add_mix(int x, int n);
+/* multiply fixed point x by int n -> x*n */
+int multiply_mix(int x, int n);
 
-        /* subtract int n from fixed point y -> x-n */
-        int sub_mix(int x, int n);
+/* divide fixed point x by fixed point y -> x/y*/
+int divide_fps(int x, int y);
 
-        /* multiply fixed point x by fixed point y -> x*y */
-        int multiply_fps(int x, int y);
-
-        /* multiply fixed point x by int n -> x*n */
-        int multiply_mix(int x, int n);
-
-        /* divide fixed point x by fixed point y -> x/y*/
-        int divide_fps(int x, int y);
-
-        /* divide fixed point x by int n -> x/n */
-        int divide_mix(int x, int n);
-
-};
+/* divide fixed point x by int n -> x/n */
+int divide_mix(int x, int n);
 
 #endif /* threads/fixed-point */
