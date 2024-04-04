3840926936587b4f7786d17261421a02dc632965
Sojeong <k5k5s5s5j5j5@naver.com>
2024-04-04 13:42:06 +0900

    timer.c: update mlfqs values by some timer ticks

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -172,6 +172,18 @@ timer_interrupt (struct intr_frame *args UNUSED)
   ticks++;
   thread_tick ();
   thread_wake ();
+
+  /* Mlfqs: update priority & values*/
+  if(thread_mlfqs){     // every tick
+    thread_increment_recent_cpu();  
+    if (ticks % 4 == 0){     // every 4 tick
+      thread_renew_priorities_mlfqs();
+    }
+    if (timer_ticks() % TIMER_FREQ == 0){   //every 1 second
+      thread_set_load_avg();
+      thread_renew_recent_cpus();
+    }
+  }
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
